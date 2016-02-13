484180f7eea683ae38b1391379d682190b52e94d
Paul Gaffney <pgaffney2000@gmail.com>
2016-02-13 16:46:20 -0500

    initial commit

---
+++ .bash_history
@@ -0,0 +1,62 @@
+sudo deluser --remove-all-files pi
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install nginx php5-fpm php5-curl php5-gd php5-cli php5-mcrypt php5-mysql php-apc mysql-server
+sudo nano /etc/nginx/nginx.conf
+sudo nano /etc/nginx/fastcgi_params
+sudo nano /etc/php5/fpm/pool.d/www.conf
+sudo mysql_secure_installation
+cd /
+cd home
+ls
+cd ADMIN
+ls
+pip install speedtest-cli
+sudo pip install speedtest-cli
+speedtest-cli -h
+speedtest-cli
+speedtest-cli --simple
+cd /
+ls
+cd /usr
+ls
+cd /home
+ls
+cd ADMIN
+ls
+sudo nano speedtest.py
+python speetest
+python speedtest.py
+sudo nano speedtest.py
+python speedtest.py
+/home
+ls
+speedtest-cli --simple
+sudo nano speedtest.py
+python speedtest.py
+sudo nano speedtest.py
+python speedtest.py
+sudo nano speedtest.py
+python speedtest.py
+sudo nano speedtest.py
+python speedtest.py
+sudo nano speedtest.py
+python speedtest.py
+sudo nano speedtest.py
+python speedtest.py
+python help
+python
+pypm
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-setuptools
+sudo easy_install pip
+sudo pip install twython
+sudo nano speedtest.py
+python
+python speedtest.py
+sudo nano speedtest.py
+shutdown -?
+shutdown help
+man shutdown
+logout
---
+++ speedtest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+import os
+import sys
+import csv
+import datetime
+import time
+from twython import Twython
+
+def test():
+
+        #run speedtest-cli
+        print '  running speedtest'
+        speedtestResults = os.popen("speedtest-cli --simple").read()
+        print '  speedtest complete'
+        #split the 3 line result (ping,down,up)
+        testResultsLines = speedtestResults.split('\n')
+
+        currentTime = time.time()
+        date = datetime.datetime.fromtimestamp(currentTime).strftime('%Y-%m-%d %H:%M:%S')
+        
+        #if speedtest could not connect set the speeds to 0
+        if "Cannot" in speedtestResults:
+                ping = 100
+                dowload = 0
+                upload = 0
+
+        #extract the values for ping down and up values
+        else:
+                ping = testResultsLines[0][6:11]
+                download = testResultsLines[1][10:14]
+                upload = testResultsLines[2][8:12]
+
+        print '  speedtest results=', date, ' ping=', ping, ' download=', download, ' upload=', upload
+
+        #append the data to file
+        print '  writing to file'
+        outputFile = open('/var/www/assets/speedtest-data.csv', 'a')
+        writer = csv.writer(outputFile)
+        writer.writerow((currentTime*1000,ping,download,upload))
+        outputFile.close()
+        print '  write complete'
+
+        #connect to twitter
+        CONSUMER_KEY = '2NdKpaNZNRJSsaIgzvGJ0MrRA'
+        CONSUMER_SECRET = 'CbJs1cMwDNSiiPWnGdEWMt9xB7bONRwE7kz5eMZzfFSKxsK1Ge'
+        ACCESS_KEY = '19665109-e9xQmFkKiifQVyHvf9PHofpZZgMANbKSQ5vSdLpOg'
+        ACCESS_SECRET = 'sft9DieLsRCwXD8oounOMUJy3gLNiPkFPZxfwg9ecriab'
+        api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+
+        #tweet
+        #api.update_status(status='Test status')
+
+        #try to tweet if speedtest couldnt even connect. Probably wont work if the internet is down
+        #if "Cannot" in a:
+        #        try:
+        #                tweet="Hey @Comcast @ComcastCares why is my internet down? I pay for 150down\\10up in Washington DC? #comcastoutage #comcast"
+        #                twit.statuses.update(status=tweet)
+        #        except:
+        #                pass
+
+        # tweet if down speed is less than whatever I set
+        #elif eval(d)<50:
+        #        print "trying to tweet"
+        #        try:
+        #                # i know there must be a better way than to do (str(int(eval())))
+        #                tweet="Hey @Comcast why is my internet speed " + str(int(eval(d))) + "down\\" + str(int(eval(u))) + "up when I pay for 150down\\10up in Washington DC? @ComcastCares @xfinity #comcast #speedtest"
+        #                twit.statuses.update(status=tweet)
+        #        except Exception,e:
+        #                print str(e)
+        #                pass
+        return
+        
+if __name__ == '__main__':
+        print 'starting test cycle...'
+        test()
+        print 'test cycle completed'
