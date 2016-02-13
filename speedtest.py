0ece0ab01b982fe10bb8039232b28d1c3f0587af
Paul Gaffney <pgaffney2000@gmail.com>
2016-02-13 17:26:01 -0500

    code cleanup - PEP 8 naming conventions

--- speedtest.py
+++ speedtest.py
@@ -6,6 +6,12 @@ import time
 
 from twython import Twython
 
+# twitter constants
+CONSUMER_KEY = '2NdKpaNZNRJSsaIgzvGJ0MrRA'
+CONSUMER_SECRET = 'CbJs1cMwDNSiiPWnGdEWMt9xB7bONRwE7kz5eMZzfFSKxsK1Ge'
+ACCESS_KEY = '19665109-e9xQmFkKiifQVyHvf9PHofpZZgMANbKSQ5vSdLpOg'
+ACCESS_SECRET = 'sft9DieLsRCwXD8oounOMUJy3gLNiPkFPZxfwg9ecriab'
+
 
 def test():
     # run speedtest-cli
@@ -41,11 +47,7 @@ def test():
     print '  write complete'
 
     # connect to twitter
-    CONSUMER_KEY = '2NdKpaNZNRJSsaIgzvGJ0MrRA'
-    CONSUMER_SECRET = 'CbJs1cMwDNSiiPWnGdEWMt9xB7bONRwE7kz5eMZzfFSKxsK1Ge'
-    ACCESS_KEY = '19665109-e9xQmFkKiifQVyHvf9PHofpZZgMANbKSQ5vSdLpOg'
-    ACCESS_SECRET = 'sft9DieLsRCwXD8oounOMUJy3gLNiPkFPZxfwg9ecriab'
-    api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+    # api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
     # tweet
     # api.update_status(status='Test status')
@@ -53,7 +55,7 @@ def test():
     # try to tweet if speedtest couldnt even connect. Probably wont work if the internet is down
     # if "Cannot" in a:
     #        try:
-    #                tweet="Hey @Comcast @ComcastCares why is my internet down? I pay for 150down\\10up in Washington DC? #comcastoutage #comcast"
+    #                tweet="Hey @Comcast @ComcastCares? #comcastoutage #comcast"
     #                twit.statuses.update(status=tweet)
     #        except:
     #                pass
@@ -63,7 +65,7 @@ def test():
     #        print "trying to tweet"
     #        try:
     #                # i know there must be a better way than to do (str(int(eval())))
-    #                tweet="Hey @Comcast why is my internet speed " + str(int(eval(d))) + "down\\" + str(int(eval(u))) + "up when I pay for 150down\\10up in Washington DC? @ComcastCares @xfinity #comcast #speedtest"
+    #                tweet="Hey @Comcast why  " + str(int(eval(d))) + "down\\" + str(int(eval(u)))
     #                twit.statuses.update(status=tweet)
     #        except Exception,e:
     #                print str(e)
